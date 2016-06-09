c4dc24453fd79bd5cbba2b9f9ff949f426575aee
iostash <iostashvxi@gmail.com>
2016-06-09 13:39:11 +0300

    added tests for _pathJoin method

---
+++ staging/amid/z.test/Path.path.test.s
@@ -0,0 +1,147 @@
+( function( ) {
+
+  'use strict';
+
+  if( typeof module !== undefined )
+  {
+
+    try
+    {
+      require( '../ServerTools.ss' );
+    }
+    catch( err )
+    {
+    }
+
+    try
+    {
+      require( '../wTools.ss' );
+    }
+    catch( err )
+    {
+      require( 'wTools' );
+    }
+
+    try
+    {
+      require( 'wTesting' );
+    }
+    catch( err )
+    {
+      require( 'include/abase/object/Testing.debug.s' );
+    }
+
+    require( '../file/Files.ss' );
+
+  }
+
+  _global_.wTests = typeof wTests === 'undefined' ? {} : wTests;
+
+  var _ = wTools;
+  var Self = {};
+
+  //
+
+  var _pathJoin = function( test )
+  {
+    var options1 =
+      {
+        reroot: 1,
+        url: 0
+      },
+      options2 =
+      {
+        reroot: 0,
+        url: 1
+      },
+      options3 =
+      {
+        reroot: 0
+      },
+
+      paths1 = [ 'http://www.site.com:13/', 'bar', 'foo', ],
+      paths2 = [ 'c:\\', 'foo\\', 'bar\\' ],
+      paths3 = [ '/bar/', '/', 'foo/' ],
+      paths4 = [ '/bar/', '/baz', 'foo/' ],
+
+      expected1 = 'http://www.site.com:13/bar/foo',
+      expected2 = 'c:/foo/bar/',
+      expected3 = '/foo/',
+      expected4 = '/bar/baz/foo/';
+
+
+    test.description = 'join url';
+    var got = _._pathJoin( paths1, options2 );
+    test.contain( got, expected1 );
+
+    test.description = 'join windows os paths';
+    var got = _._pathJoin( paths2, options3 );
+    test.contain( got, expected2 );
+
+    test.description = 'join unix os paths';
+    var got = _._pathJoin( paths3, options3 );
+    test.contain( got, expected3 );
+
+    test.description = 'join unix os paths with reroot';
+    var got = _._pathJoin( paths4, options1 );
+    test.contain( got, expected4 );
+
+    if( Config.debug )
+    {
+
+      test.description = 'missed arguments';
+      test.shouldThrowError( function()
+      {
+        _._pathJoin();
+      });
+
+      test.description = 'path element is not string';
+      test.shouldThrowError( function()
+      {
+        _._pathJoin( [ 34 , 'foo/' ], options3 );
+      });
+
+      test.description = 'missed options';
+      test.shouldThrowError( function()
+      {
+        _._pathJoin( paths1 );
+      });
+
+      test.description = 'options has unexpected parameters';
+      test.shouldThrowError( function()
+      {
+        _._pathJoin( paths1, { wrongParameter: 1 } );
+      });
+
+    }
+
+
+  };
+
+  //
+
+  // --
+  // proto
+  // --
+
+  var Proto =
+  {
+
+    name : 'PathTest',
+
+    tests:
+    {
+
+      _pathJoin: _pathJoin,
+
+    },
+
+    verbose : 1
+
+  };
+
+  Self.__proto__ = Proto;
+  wTests[ Self.name ] = Self;
+  _.testing.test( Self );
+
+} )( );

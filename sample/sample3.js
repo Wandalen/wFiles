ad6011940eb33c57a3cf90fe3d81f5879cd6b428
volodymyrmigdal <volodymyr.migdal@gmail.com>
2016-09-29 18:59:20 +0300

    sample update

--- sample/sample3.js
+++ sample/sample3.js
@@ -2,7 +2,9 @@ if( typeof module !== 'undefined' )
 {
   // require( '../../wTools/staging/abase/wTools.s' )
   require( 'wTools' )
-  require( '../../wFiles/staging/amid/file/Files.ss' )
+  require( '../staging/amid/file/Files.ss' )
+  require( '../staging/amid/file/FileProviderSimpleStructure.ss' )
+
 
 
 }
@@ -16,5 +18,5 @@ var _ = wTools;
 //
 // logger.log( 'treeWriten :',_.toStr( treeWriten,{ levels : 99 } ) );
 //
-var fileProvider = _.FileProvider.HardDrive({});
-// var fileProvider = _.FileProvider.SimpleStructure({});
+// var fileProvider = _.FileProvider.HardDrive({});
+var fileProvider = _.FileProvider.SimpleStructure( { tree : {} } );

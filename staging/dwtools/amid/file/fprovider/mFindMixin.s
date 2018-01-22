950c09a0c28f50d6bf78eaddb8a70f0a268ead0c
volodymyrmigdal <volodymyr.migdal@gmail.com>
2018-01-22 14:55:47 +0200

    Fixed filesFind, adjusted filesFind test

--- staging/dwtools/amid/file/fprovider/mFindMixin.s
+++ staging/dwtools/amid/file/fprovider/mFindMixin.s
@@ -307,11 +307,12 @@ function filesFind()
 
   function forFilePath( filePath,o )
   {
+    var dir = filePath;
 
     if( self.fileIsTerminal( filePath ) || self.fileIsSoftLink( filePath ) ) /* what for? */
-    filePath = _.pathDir( filePath );
+    dir = _.pathDir( filePath );
 
-    var recordOptions = _.FileRecordOptions.tollerantMake( o,{ dir : filePath } );
+    var recordOptions = _.FileRecordOptions.tollerantMake( o,{ dir : dir } );
     var record = self.fileRecord( filePath,recordOptions );
 
     debugger;
@@ -371,7 +372,6 @@ function filesFind()
       forDirectory( subdirRecord,o );
     }
 
-    if(  )
     if( o.includingDirectories )
     _.routinesCall( o,o.onDown,[ dirRecord ] );
 

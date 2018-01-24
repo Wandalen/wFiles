8c6dad07d0d64a841b3414b04541ba88a346407e
volodymyr.migdal <volodymyr.migdal@gmail.com>
2018-01-24 13:40:40 +0200

    adjust sample

--- sample/fileTimeSet.js
+++ sample/fileTimeSet.js
@@ -1,3 +1,4 @@
+
 if( typeof module !== 'undefined' )
 require( 'wFiles' )
 
@@ -7,20 +8,19 @@ var filePath = _.pathJoin( __dirname, 'file' );
 
 _.fileProvider.fileWrite( filePath, 'data' );
 
-var newMtime = new Date( 1516791641389 );
-var newAtime = new Date( 1516791641389 );
+var statBefore = _.fileProvider.fileStat( filePath );
 
-_.fileProvider.fileTimeSet( filePath, newAtime, newMtime );
+_.fileProvider.fileTimeSet( filePath, statBefore.atime, statBefore.mtime );
 
 var statAfter = _.fileProvider.fileStat( filePath );
 
-var mtimeIdentical =  statAfter.mtime.getTime() === newMtime.getTime();
+var mtimeIdentical =  statAfter.mtime.getTime() === statBefore.mtime.getTime();
 _.EPS = 500;
-var mtimeEquivalent =  _.numbersAreEquivalent( statAfter.mtime.getTime(),newMtime.getTime() )
+var mtimeEquivalent =  _.numbersAreEquivalent( statAfter.mtime.getTime(),statBefore.mtime.getTime() )
 
 console.log( "mtimeIdentical: ", mtimeIdentical )
 console.log( "mtimeEquivalent: ", mtimeEquivalent )
 console.log( "statAfter.mtime: ", statAfter.mtime.getTime() )
-console.log( "newMtime: ", newMtime.getTime() )
+console.log( "statBefore: ", statBefore.mtime.getTime() )
 
 debugger

0c27b9771d03fc3ca7a247034ce7a7ab36e97a0f
Wandalen <wandalen.me@gmail.com>
2018-05-08 14:57:31 +0300

    sync

--- staging/dwtools/amid/file/fprovider/pSimpleStructure.s
+++ staging/dwtools/amid/file/fprovider/pSimpleStructure.s
@@ -1106,15 +1106,14 @@ function filesTreeRead( o )
       }
       else if( o.readingTerminals )
       {
-        if( !o.resolvingSoftLink || !o.resolvingTextLink )
-        if( o.srcProvider.fileIsSoftLink
-        ({
-          filePath : record.absolute,
-          resolvingSoftLink : o.resolvingSoftLink,
-          resolvingTextLink : o.resolvingTextLink,
-          usingTextLink : o.usingTextLink,
-        }));
-        element = null;
+        // if( o.srcProvider.fileIsSoftLink
+        // ({
+        //   filePath : record.absolute,
+        //   resolvingSoftLink : o.resolvingSoftLink,
+        //   resolvingTextLink : o.resolvingTextLink,
+        //   usingTextLink : o.usingTextLink,
+        // }))
+        // element = null;
         _.assert( _.boolLike( o.readingTerminals ),'unknown value of { o.readingTerminals }',_.strQuote( o.readingTerminals ) );
         if( element === undefined )
         element = o.srcProvider.fileReadSync( record.absolute );

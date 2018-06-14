997a843f5388dcd51f5b7d4b07a87b03529488d8
vmigdal <volodymyr.migdal@gmail.com>
2018-06-14 13:14:53 +0300

    test

--- sample/statsDoc.js
+++ sample/statsDoc.js
@@ -5,7 +5,7 @@ require( 'wFiles' )
 var _ = wTools;
 var waitSync = require( 'wait-sync' );
 
-function showStats( s )
+function showStats( s,o )
 {
     console.log( '\nstats.atime: ', s.atime );
     console.log( 'stats.atime: ', s.atime.getTime() );
@@ -18,6 +18,23 @@ function showStats( s )
 
     console.log( '\nstats.birthtime: ', s.birthtime );
     console.log( 'stats.birthtime: ', s.birthtime.getTime() );
+
+    if( o )
+    {
+       console.log( '\n' )
+
+       if( s.atime.getTime() !== o.atime.getTime() )
+       console.log( '   atime changed' );
+
+       if( s.mtime.getTime() !== o.mtime.getTime() )
+       console.log( '   mtime changed' );
+
+       if( s.ctime.getTime() !== o.ctime.getTime() )
+       console.log( '   ctime changed' );
+
+       if( s.birthtime.getTime() !== o.birthtime.getTime() )
+       console.log( '   stime changed' );
+    }
 }
 
 var testDir = _.pathJoin( __dirname, 'statsDoc' );
@@ -34,25 +51,25 @@ _.fileProvider.filesDelete( testDir );
 _.fileProvider.fileWrite( testFile, testFile );
 
 console.log( "\n----> Stats of the file before read:" )
-var stats = _.fileProvider.fileStat( testFile );
-showStats( stats );
+var ostats = _.fileProvider.fileStat( testFile );
+showStats( ostats );
 
 console.log( "\n----> After read with no delay :" );
 _.fileProvider.fileRead( testFile );
 var stats = _.fileProvider.fileStat( testFile );
-showStats( stats );
+showStats( stats,ostats );
 
 console.log( "\n----> After read with 10ms delay :" );
 waitSync( 0.01 );
 _.fileProvider.fileRead( testFile );
 var stats = _.fileProvider.fileStat( testFile );
-showStats( stats );
+showStats( stats,ostats );
 
 console.log( "\n----> After read with 1000ms delay :" );
 waitSync( 1 );
 _.fileProvider.fileRead( testFile );
 var stats = _.fileProvider.fileStat( testFile );
-showStats( stats );
+showStats( stats,ostats );
 
 //
 
@@ -61,20 +78,20 @@ _.fileProvider.filesDelete( testDir );
 _.fileProvider.fileWrite( testFile, testFile );
 
 console.log( "\n----> Stats of the file before content change:" )
-var stats = _.fileProvider.fileStat( testFile );
-showStats( stats );
+var ostats = _.fileProvider.fileStat( testFile );
+showStats( ostats );
 
 _.fileProvider.fileWrite( testFile, testFile + testFile );
 
 console.log( "\n----> Stats of the file after content change, without delay:" )
 var stats = _.fileProvider.fileStat( testFile );
-showStats( stats );
+showStats( stats,ostats );
 
 console.log( "\n----> Stats of the file after content change, with 10 ms delay:" )
 waitSync( 0.01 )
 _.fileProvider.fileWrite( testFile, 'dasd' );
 var stats = _.fileProvider.fileStat( testFile );
-showStats( stats );
+showStats( stats,ostats );
 
 //
 
@@ -101,26 +118,30 @@ console.log( "\n--> Copy file, rewriting dst:" )
 
 var testFile2 = _.pathJoin( testDir, 'file2' );
 _.fileProvider.filesDelete( testDir );
-_.fileProvider.fileWrite( testFile, testFile );
-_.fileProvider.fileWrite( testFile2, testFile2 );
+_.fileProvider.fileWrite( testFile, 'abc' );
+waitSync( 0.1 )
+_.fileProvider.fileWrite( testFile2, 'cda' );
 
 console.log( "\n----> Stats of src before copy:" )
-var stats = _.fileProvider.fileStat( testFile );
-showStats( stats );
+var ostatsSrc = _.fileProvider.fileStat( testFile );
+showStats( ostatsSrc );
 
 console.log( "\n----> Stats of dst before copy:" )
-var stats = _.fileProvider.fileStat( testFile2 );
-showStats( stats );
+var ostatsDst = _.fileProvider.fileStat( testFile2 );
+showStats( ostatsDst );
 
-_.fileProvider.fileCopy( testFile2, testFile );
+waitSync( 0.01 )
+
+var fs = require( 'fs' )
+fs.copyFileSync( _.fileProvider.pathNativize( testFile ), _.fileProvider.pathNativize( testFile2 ) );
 
 console.log( "\n----> Stats of src after copy:" )
 var stats = _.fileProvider.fileStat( testFile );
-showStats( stats );
+showStats( stats,ostatsSrc );
 
 console.log( "\n----> Stats of dst after copy:" )
 var stats = _.fileProvider.fileStat( testFile2 );
-showStats( stats );
+showStats( stats,ostatsDst );
 
 //
 
@@ -129,29 +150,55 @@ _.fileProvider.filesDelete( testDir );
 _.fileProvider.fileWrite( testFile, testFile );
 
 console.log( "\n----> Stats of the file before changes:" )
-var stats = _.fileProvider.fileStat( testFile );
-showStats( stats );
+var ostats = _.fileProvider.fileStat( testFile );
+showStats( ostats );
 
 console.log( "\n----> Setting same atime/mtime to check precision:" );
 _.fileProvider.fileTimeSet( testFile, stats.atime, stats.mtime );
 var stats1 = _.fileProvider.fileStat( testFile );
-showStats( stats1 );
+showStats( stats1,ostats );
 
-console.log( "\n----> Diff atime:" );
+console.log( "\n-----> Diff atime:" );
 console.log( stats1.atime.getTime() - stats.atime.getTime() )
-console.log( "\n----> Diff mtime:" );
+console.log( "\n-----> Diff mtime:" );
 console.log( stats1.mtime.getTime() - stats.mtime.getTime() )
 
-console.log( "\n----> Adding 1 second to original atime/mtime:" );
+console.log( "\n----> Adding 10ms to original atime/mtime:" );
+var atime = new Date( stats.atime.getTime() + 10 )
+var mtime = new Date( stats.mtime.getTime() + 10 )
+
+_.fileProvider.fileTimeSet( testFile, atime, mtime );
+var stats2 = _.fileProvider.fileStat( testFile );
+showStats( stats2,ostats );
+
+console.log( "\n-----> Diff atime:" );
+console.log( stats2.atime.getTime() - stats.atime.getTime() )
+console.log( "\n-----> Diff mtime:" );
+console.log( stats2.mtime.getTime() - stats.mtime.getTime() )
+
+console.log( "\n----> Adding 100ms to original atime/mtime:" );
+var atime = new Date( stats.atime.getTime() + 100 )
+var mtime = new Date( stats.mtime.getTime() + 100 )
+
+_.fileProvider.fileTimeSet( testFile, atime, mtime );
+var stats2 = _.fileProvider.fileStat( testFile );
+showStats( stats2,ostats );
+
+console.log( "\n-----> Diff atime:" );
+console.log( stats2.atime.getTime() - stats.atime.getTime() )
+console.log( "\n-----> Diff mtime:" );
+console.log( stats2.mtime.getTime() - stats.mtime.getTime() )
+
+console.log( "\n----> Adding 1000ms to original atime/mtime:" );
 var atime = new Date( stats.atime.getTime() + 1000 )
 var mtime = new Date( stats.mtime.getTime() + 1000 )
 
 _.fileProvider.fileTimeSet( testFile, atime, mtime );
 var stats2 = _.fileProvider.fileStat( testFile );
-showStats( stats2 );
+showStats( stats2,ostats );
 
-console.log( "\n----> Diff atime:" );
+console.log( "\n-----> Diff atime:" );
 console.log( stats2.atime.getTime() - stats.atime.getTime() )
-console.log( "\n----> Diff mtime:" );
+console.log( "\n-----> Diff mtime:" );
 console.log( stats2.mtime.getTime() - stats.mtime.getTime() )
 

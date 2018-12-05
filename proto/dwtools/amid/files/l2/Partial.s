bf0cacc3fe6b95c1d48c316a0265a097d7bdf2a9
vmigdal <volodymyr.migdal@gmail.com>
2018-12-05 19:22:52 +0200

    isTerminal : fix cases with self links

--- proto/dwtools/amid/files/l2/Partial.s
+++ proto/dwtools/amid/files/l2/Partial.s
@@ -3609,15 +3609,29 @@ function isTerminal_body( o )
   _.assert( _.boolLike( o.resolvingSoftLink ) );
   _.assert( _.boolLike( o.resolvingTextLink ) );
 
-  o.filePath = self.pathResolveLink
+  let stat = self.statReadAct
   ({
+    filePath : o.filePath,
+    throwing : 0,
+    sync : 1,
+    resolvingSoftLink : 0,
+  });
+
+  let o2 =
+  {
     filePath : o.filePath,
     resolvingSoftLink : o.resolvingSoftLink,
     resolvingTextLink : o.resolvingTextLink,
+    stat : stat,
     throwing : 0
-  });
+  }
+
+  o.filePath = self.pathResolveLink( o2 );
 
   if( o.filePath === null )
+  return false;
+
+  if( o2.stat === null )
   return false;
 
   // if( self.isDir( o.filePath ) )
@@ -3638,17 +3652,17 @@ function isTerminal_body( o )
   // if( _.routineIs( self.isTerminalAct ) )
   // return self.isTerminalAct( _.mapOnly( o, self.isTerminalAct.defaults ) );
 
-  let stat = self.statRead
-  ({
-    filePath : o.filePath,
-    resolvingSoftLink : 0,
-    resolvingTextLink : 0,
-  });
+  // let stat = self.statRead
+  // ({
+  //   filePath : o.filePath,
+  //   resolvingSoftLink : 0,
+  //   resolvingTextLink : 0,
+  // });
 
-  if( !stat )
-  return false;
+  // if( !stat )
+  // return false;
 
-  return stat.isTerminal();
+  return o2.stat.isTerminal();
 }
 
 var defaults = isTerminal_body.defaults = Object.create( null );

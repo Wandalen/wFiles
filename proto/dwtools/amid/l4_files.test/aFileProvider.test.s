3a0aca6f9bb172dcc096d6061c01f96f63f01f60
wandalen <wandalen.me@gmail.com>
2020-06-25 17:15:21 +0300

    fix hardLinkReturnSync

--- proto/dwtools/amid/l4_files.test/aFileProvider.test.s
+++ proto/dwtools/amid/l4_files.test/aFileProvider.test.s
@@ -49790,31 +49790,25 @@ hardLinkExperiment.experimental = 1;
 
 //
 
-// передбачити всі ключові сценарії, без зайвих, при яких повертається true або false
-
 function hardLinkReturnSync( test )
 {
   let context = this;
   let a = context.assetFor( test, false ); /* qqq3 */
 
-  //
+  /* */
 
   test.case = 'hard link does not exist';
 
   a.reflect();
 
-  var srcPath = _.path.join( '/dir1/dir2', 'File1.txt' );
-  var dstPath = _.path.join( '/dir1/dir2', 'File2.txt' );
+  a.fileProvider.fileWrite( a.abs( 'src' ), 'some text' );
+  var got = a.fileProvider.hardLink( a.abs( 'dst' ), a.abs( 'src' ) );
+  test.identical( got, true );
+  test.identical( a.fileProvider.filesAreHardLinked( a.abs( 'dst' ), a.abs( 'src' ) ), true );
 
-  a.fileProvider.fileWrite( srcPath, 'some text' );
-  var got = a.fileProvider.hardLink( dstPath, srcPath );
-  test.identical( got, false );
-  test.identical( a.fileProvider.filesAreHardLinked( dstPath, srcPath ), true );
+  /* */
 
 }
-
-//
-
 
 // --
 // declare

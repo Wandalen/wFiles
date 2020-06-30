c1fb178ad4c47fd251cad661f23399f64c16fc3e
Artem Bohush <art.bohush@gmail.com>
2020-06-30 12:40:24 +0300

    linking concepts added from wPathBasic

--- proto/dwtools/amid/l4_files.test/aFileProvider.test.s
+++ proto/dwtools/amid/l4_files.test/aFileProvider.test.s
@@ -50730,6 +50730,67 @@ function textLinkReturnSync( test )
     test.identical( a.fileProvider.filesAreTextLinked({ filePath : [ a.abs( 'src' ), a.abs( 'dst' ) ] }), true );
   }
   test.close( 'dst does not exist && directory does not exist' );
+
+  /* */
+
+  test.open( 'dst does not exist && directory exists' );
+  {
+    test.case = 'rewriting : 1';
+    a.reflect();
+    a.fileProvider.fileWrite( a.abs( 'src' ), 'some text' );
+    a.fileProvider.dirMake( a.abs( 'dir1' ) );
+    var got = a.fileProvider.textLink({ dstPath : a.abs( 'dir1/dst' ), srcPath : a.abs( 'src' ), rewriting : 1 });
+    test.identical( got, true );
+    test.identical( a.fileProvider.filesAreTextLinked({ filePath : [ a.abs( 'src' ), a.abs( 'dir1/dst' ) ] }), true );
+  
+    //
+
+    test.case = 'rewriting : 0';
+    a.reflect();
+    a.fileProvider.fileWrite( a.abs( 'src' ), 'some text' );
+    a.fileProvider.dirMake( a.abs( 'dir1' ) );
+    var got = a.fileProvider.textLink({ dstPath : a.abs( 'dir1/dst' ), srcPath : a.abs( 'src' ), rewriting : 0 });
+    test.identical( got, true );
+    test.identical( a.fileProvider.filesAreTextLinked({ filePath : [ a.abs( 'src' ), a.abs( 'dir1/dst' ) ] }), true );
+
+    //
+
+    test.case = 'rewritingDirs : 1';
+    a.reflect();
+    a.fileProvider.fileWrite( a.abs( 'src' ), 'some text' );
+    a.fileProvider.dirMake( a.abs( 'dir1' ) );
+    var got = a.fileProvider.textLink({ dstPath : a.abs( 'dir1/dst' ), srcPath : a.abs( 'src' ), rewritingDirs : 1 });
+    test.identical( got, true );
+    test.identical( a.fileProvider.filesAreTextLinked({ filePath : [ a.abs( 'src' ), a.abs( 'dir1/dst' ) ] }), true );
+
+    //
+
+    // test.case = 'rewritingDirs : 0';
+    // a.reflect();
+    // a.fileProvider.fileWrite( a.abs( 'src' ), 'some text' );
+    // var got = a.fileProvider.textLink({ dstPath : a.abs( 'dst' ), srcPath : a.abs( 'src' ), rewritingDirs : 0 });
+    // test.identical( got, true );
+    // test.identical( a.fileProvider.filesAreTextLinked({ filePath : [ a.abs( 'src' ), a.abs( 'dst' ) ] }), true );
+
+    //
+
+    // test.case = 'makingDirectory : 1';
+    // a.reflect();
+    // a.fileProvider.fileWrite( a.abs( 'src' ), 'some text' );
+    // var got = a.fileProvider.textLink({ dstPath : a.abs( 'dir1/dir2/dst' ), srcPath : a.abs( 'src' ), makingDirectory : 1 });
+    // test.identical( got, true );
+    // test.identical( a.fileProvider.filesAreTextLinked({ filePath : [ a.abs( 'src' ), a.abs( 'dir1/dir2/dst' ) ] }), true );
+
+    //
+
+    // test.case = 'makingDirectory : 0';
+    // a.reflect();
+    // a.fileProvider.fileWrite( a.abs( 'src' ), 'some text' );
+    // var got = a.fileProvider.textLink({ dstPath : a.abs( 'dst' ), srcPath : a.abs( 'src' ), makingDirectory : 0 });
+    // test.identical( got, true );
+    // test.identical( a.fileProvider.filesAreTextLinked({ filePath : [ a.abs( 'src' ), a.abs( 'dst' ) ] }), true );
+  }
+  test.close( 'dst does not exist && directory exists' );
 }
 
 // --

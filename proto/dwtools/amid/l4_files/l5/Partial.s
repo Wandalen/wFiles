c87241e0035a3163027b8c6586408773af82ed6e
Artem Bohush <art.bohush@gmail.com>
2020-07-03 13:08:26 +0300

    hardLink, bug fixed 2.0

--- proto/dwtools/amid/l4_files/l5/Partial.s
+++ proto/dwtools/amid/l4_files/l5/Partial.s
@@ -7278,13 +7278,13 @@ function areHardLinked_body( o ) /* qqq : refactor. probably move some code to a
   if( !o.filePath.length )
   return true;
 
+  if( o.filePath.filter( ( path ) => !self.fileExists( path ) ).length )
+  return false;
+
   let result;
 
   for( let i = 1 ; i < o.filePath.length ; i++ )
   {
-    if( o.filePath[ 0 ] === o.filePath[ i ] && o.filePath.filter( ( path ) => !self.fileExists( path ) ).length )
-    return false;
-
     result = self.areHardLinkedAct({ filePath : [ o.filePath[ 0 ], o.filePath[ i ] ] });
     _.assert( _.boolIs( result ) || result === _.maybe );
     if( !result )

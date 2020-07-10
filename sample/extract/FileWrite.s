6de9c7be8e49441b4ae6f9fe708f1d51f5dffa60
Artem Bohush <art.bohush@gmail.com>
2020-07-10 09:43:29 +0300

    Extract, FileWrite.s sample refactored

---
+++ sample/extract/FileWrite.s
@@ -0,0 +1,46 @@
+
+if( typeof module !== 'undefined' )
+require( 'wFiles' )
+
+var tree =
+{
+  'dir1' :
+  {
+    'dir2' :
+    {
+      'file.txt' : 'previous data...',
+    }
+  }
+}
+
+//
+
+var _ = wTools;
+var provider = _.FileProvider.Extract({ filesTree : tree });
+
+// fileRead sync
+
+provider.fileWrite
+({
+  filePath : '/dir1/dir2/file.txt',
+  data : 'new sync data...',
+  sync : 1
+});
+
+var result = provider.fileRead({ filePath : '/dir1/dir2/file.txt', sync : 1 });
+console.log( result ); // logs: new sync data...
+
+//fileRead async
+
+provider.fileWrite
+({
+  filePath : '/dir1/dir2/file.txt',
+  data : 'new async data...',
+  sync : 0
+})
+.finallyGive( ( err, arg ) =>
+{
+  if( err ) throw err;
+  var result = provider.fileRead({ filePath : '/dir1/dir2/file.txt', sync : 1 });
+  console.log( result ); // logs: new async data...
+});

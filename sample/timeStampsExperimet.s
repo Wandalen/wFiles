14ed7adecd73c466f0553fd3de5036b9ba462645
Artem Bohush <art.bohush@gmail.com>
2020-07-10 14:40:15 +0300

    TimeStampsExperimet, refactored sample

--- sample/timeStampsExperimet.s
+++ sample/timeStampsExperimet.s
@@ -6,29 +6,29 @@ var _ = wTools;
 
 var fs = require( 'fs' );
 
-var file = _.path.s.join(  __dirname, 'file' );
+var file = _.path.s.join( __dirname, 'file' );
 
 _.fileProvider.fileWrite( file, file );
 
-var fileNative = _.fileProvider.path.s.nativize( file );
+var fileNative = _.fileProvider.path.nativize( file );
 
 var time = new Date( Date.now() );
 
-// var fd = fs.openSync( fileNative, 'w' );
-// fs.futimesSync( fd, time, time );
+var fd = fs.openSync( fileNative, 'w' );
+fs.futimesSync( fd, time, time );
 
-// console.log( '\n------------------------\n' );
+console.log( '\n------------------------\n' );
 
-// var stat = _.fileProvider.fileStat( fileNative );
-// console.log( time.getTime() )
-// console.log( stat.mtime.getTime() )
-// console.log( stat.mtime.getTime() - time.getTime() );
+var stat = _.fileProvider.statRead( fileNative );
+console.log( time.getTime() )
+console.log( stat.mtime.getTime() )
+console.log( stat.mtime.getTime() - time.getTime() );
 
 
 console.log( '\n------------------------\n' );
 
 _.fileProvider.timeWrite( file, time, time );
-var stat = _.fileProvider.fileStat( fileNative );
+var stat = _.fileProvider.statRead( fileNative );
 console.log( time.getTime() )
 console.log( stat.mtime.getTime() )
 console.log( stat.mtime.getTime() - time.getTime() );

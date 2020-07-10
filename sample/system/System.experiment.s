ba96b4bac1180cf9f41ba82e3314555d609f8f7d
Artem Bohush <art.bohush@gmail.com>
2020-07-10 11:38:41 +0300

    System, refactored sample

--- sample/system/System.experiment.s
+++
@@ -1,7 +0,0 @@
-
-if( typeof module !== 'undefined' )
-require( 'wFiles' )
-
-var _ = wTools;
-
-var hub = _.FileProvider.Hub({ empty : 1 });
---
+++ sample/system/System.s
@@ -0,0 +1,40 @@
+if( typeof module !== 'undefined' )
+require( 'wFiles' )
+
+var _ = wTools;
+
+/* fileCopy from HardDrive to Extract */
+
+var hub = _.FileProvider.System({ providers : [] });
+
+var hardDriveProvider = _.FileProvider.HardDrive({ protocol : 'hard' });
+
+var tree =
+{
+  dir1 :
+  {
+    dir2 : {}
+  }
+}
+var extractProvider = _.FileProvider.Extract({ protocol : 'ext', filesTree : tree });
+
+hub.providerRegister( hardDriveProvider );
+hub.providerRegister( extractProvider );
+/* or vice versa
+hardDriveProvider.providerRegisterTo( hub );
+extractProvider.providerRegisterTo( hub );
+*/
+
+hub.fileCopy
+({
+  dstPath : 'ext:///dir1/dir2/fileFromHardDrive.txt',
+  srcPath : `hard://${__dirname}/fileOnHardDrive.txt`,
+  sync : 1
+});
+
+var data = extractProvider.fileRead
+({
+  filePath : '/dir1/dir2/fileFromHardDrive.txt',
+  sync : 1
+});
+console.log( data ); // logs: data from hard drive...
